10746040b3acdedb3d9a644264a6d86eef56e625
Benjamin Kay <ben.kay01@hotmail.co.uk>
2019-10-30 16:07:57 +0000

    fix images and sw event listener

--- pwa/service-worker.js
+++ pwa/service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(event) {
   console.log('Service Worker installed')
 });
 
-self.addEventListener('activated', function(event) {
+self.addEventListener('activate', function(event) {
   console.log('Service Worker activated')
 });
 
